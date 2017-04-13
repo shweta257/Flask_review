835d92abf7b7c46a7bfa61fd57377275968fdf6b
Shweta Singhal <shweta@cs.utah.edu>
2017-04-12 19:02:07 -0600

    added login feature

--- app.py
+++ app.py
@@ -1,12 +1,32 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, Response, abort
 from flask_sqlalchemy import SQLAlchemy
 from flask_script import Manager
+from flask.ext.login import LoginManager, UserMixin, login_required, login_user, logout_user
 import os, sys
+
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = "login"
 manager = Manager(app)
-tasks = []
+
+app.config['SECRET_KEY'] = 'FLASK EXAMPLE'
+
+
+class User(UserMixin):
+    def __init__(self, id):
+        self.id = id
+        self.name = "user" + str(id)
+        self.password = self.name + "_pass"
+
+    def __repr__(self):
+        return "%d/%s/%s" % (self.id, self.name, self.password)
+
+
+users = [User(id) for id in range(1, 2)]
+
 
 class Task(db.Model):
     __tablename__ = 'tasks'
@@ -14,12 +34,50 @@ class Task(db.Model):
     message = db.Column(db.Text)
     date = db.Column(db.Text)
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if password == username + "_pass":
+            id = username.split('user')[1]
+            user = User(id)
+            login_user(user)
+            return redirect('/')
+        else:
+            return abort(401)
+    else:
+        return Response('''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+        ''')
+
+@login_manager.request_loader
+def load_user(request):
+    token = request.headers.get('Authorization')
+    if token is None:
+        token = request.args.get('token')
+
+    if token is not None:
+        username,password = token.split(":") # naive token
+        user_entry = User.get(username)
+        if (user_entry is not None):
+            user = User(user_entry[0],user_entry[1])
+            if (user.password == password):
+                return user
+    return None
+
 @app.route('/')
+@login_required
 def index():
     tasks = Task.query.all()
     return render_template('tasks.html', tasks=tasks)
 
 @app.route('/delete/<i>/')
+@login_required
 def delete(i):
     task = Task.query.get_or_404(i)
     if task is None:
@@ -29,6 +87,7 @@ def delete(i):
     return redirect('/')
 
 @app.route('/add/', methods=['POST'])
+@login_required
 def add():
     if request.method == 'POST':
         message = request.form.get('message')
@@ -39,6 +98,7 @@ def add():
     return redirect('/')
 
 @app.route('/edit/<i>/', methods=['GET','POST'])
+@login_required
 def edit(i):
     if request.method == 'POST':
         task = Task.query.get_or_404(i)
@@ -56,6 +116,21 @@ def edit(i):
         return render_template('task_edit.html', task=task)
 
 
+
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect('/login/')
+
+# handle login failed
+@app.errorhandler(401)
+def page_not_found(e):
+    return Response('<p>Login failed</p>')
+
+@login_manager.user_loader
+def load_user(userid):
+    return User(userid)
 
 if __name__ == '__main__':
     basedir = os.path.abspath(os.path.dirname(__file__))
